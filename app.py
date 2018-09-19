699d676e9a477b6d463456d969116b39e52182ea
legion <test@test.com>
2018-09-19 21:51:52 +0530

    Add Post Feature Added and Article Data can stored on Mysql Database

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, url_for, redirect,session
 from flask import render_template
 from functools import wraps
 from flask_mysqldb import MySQL
-from form import register_form
+from form import register_form,post_form
 app = Flask(__name__)
 mysql = MySQL(app)
 
@@ -12,7 +12,7 @@ app.config['SECRET_KEY'] = '112345678'
 # DB config
 app.config['MYSQL_HOST'] ='localhost'
 app.config['MYSQL_USER'] ='root'
-app.config['MYSQL_PASSWORD'] =''
+app.config['MYSQL_PASSWORD'] ='password'
 app.config['MYSQL_DB'] ='flaskapp'
 app.config['MYSQL_CURSORCLASS'] ='DictCursor' #For returning Dictionary type data
 
@@ -92,28 +92,41 @@ def is_logged_in(f):
 @app.route('/profile',methods =['POST','GET'])
 @is_logged_in
 def profile():
-	return render_template('profile.html')
+	postinfo = request.args.get('postinfo')
+	return render_template('profile.html', postinfo = postinfo)
+
+#-------------------------------Add Post------------------------------------
+
+@app.route('/add_post', methods = ['POST','GET'])
+@is_logged_in
+def add_post():
+	form = post_form(request.form)
+	if request.method =='POST' and form.validate():
+		title = request.form['title']
+		author = session['user']
+		content = request.form['content']
+		print(title)
+		print(author)
+		print(content)
+		cur = mysql.connection.cursor()
+		cur.execute("""INSERT INTO posts (title,author,content)
+		 VALUES(%s,%s,%s)""",(title, author,content))
+		mysql.connection.commit()
+		cur.close()
+
+		postinfo = "Your Recent Contennt is Added"
+		app.logger.info(postinfo)
+		return redirect(url_for('profile',postinfo = postinfo))
+	else:
+		app.logger.info("Form Invalid")
+		return render_template('add_post.html', form = form)	
 
 # Logout
 @app.route('/logout')
+@is_logged_in
 def logout():
 	session.clear()
 	return redirect(url_for('login'))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
--- form.py
+++ form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField, BooleanField,SubmitField
+from wtforms import StringField,TextField,PasswordField, BooleanField,SubmitField
 from wtforms.validators import DataRequired, Email, Length, EqualTo
 
 class register_form(FlaskForm):
@@ -7,4 +7,9 @@ class register_form(FlaskForm):
 	 email = StringField('Email',validators=[DataRequired(), Email()])
 	 password = PasswordField('Password', validators=[DataRequired()])
 	 c_pass = PasswordField('Confirm', validators= [DataRequired(),EqualTo('password')])
+	 submit = SubmitField('submit')
+
+class post_form(FlaskForm):
+	 title = StringField('Title',validators=[DataRequired()])
+	 content = TextField('Confirm', validators= [DataRequired()])
 	 submit = SubmitField('submit')
